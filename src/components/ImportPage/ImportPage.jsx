03796a605755d1843be8ba9ebdadf0a143c36d27
Nelya <fohado@mail.ru>
2024-12-18 16:12:03 +0300

    fix import

--- src/components/ImportPage/ImportPage.jsx
+++ src/components/ImportPage/ImportPage.jsx
@@ -1,8 +1,40 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 const ImportPage = () => {
     const [file, setFile] = useState(null);
     const [history, setHistory] = useState([]);
+
+    function replaceNullWithZero(obj) {
+        if (Array.isArray(obj)) {
+            return obj.map((item) => replaceNullWithZero(item));
+        } else if (typeof obj === 'object' && obj !== null) {
+            for (const key in obj) {
+                if (obj[key] === null) {
+                    obj[key] = 0;
+                } else if (typeof obj[key] === 'object') {
+                    obj[key] = replaceNullWithZero(obj[key]);
+                }
+            }
+        }
+        return obj;
+    }
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const goToPreviousPage = () => {
+        console.log(currentPage)
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+            fetchHistory(currentPage - 1)
+        }
+        console.log(currentPage)
+    };
+
+    const goToNextPage = () => {
+        console.log(currentPage)
+        setCurrentPage(currentPage + 1);
+        console.log(currentPage)
+        fetchHistory(currentPage + 1)
+    };
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
@@ -18,53 +50,189 @@ const ImportPage = () => {
             const response = await fetch('http://localhost:8080/is-lab1-backend-1.0-SNAPSHOT/api/import/upload', {
                 method: 'POST',
                 headers: {
-                    "Content-Type": "multipart/form-data",
                     Authorization: `Bearer ${localStorage.getItem("token")}`,
                 },
                 body: formData,
             });
             if (response.ok) {
                 alert('File uploaded successfully');
+                fetchPaginationData();
             } else {
-                alert('Upload failed');
+                const data = await response.json();
+                alert(data.message);
+                fetchPaginationData();
             }
         } catch (error) {
             console.error('Error:', error);
         }
     };
 
-    const fetchHistory = async () => {
-        const response = await fetch('http://localhost:8080/is-lab1-backend-1.0-SNAPSHOT/api/import/history',  {
+    const fetchHistory = async (page) => {
+        let filterColomn = document.getElementById("filter-coloumn");
+        let filter = document.querySelector("#filter");
+        let sorted = document.getElementById("sorted");
+        if (filterColomn && filter) {
+            filterColomn = filterColomn.value;
+            filter = filter.value;
+        }
+        if (sorted) {
+            sorted = sorted.value;
+        }
+        let url = "?";
+        if (filterColomn && filter) {
+            url += "&filter-value=" + filter + "&filter-column=" + filterColomn;
+        }
+        if (page) {
+            url += "&page=" + page;
+        }
+        if (sorted) {
+            url += "&sorted=" + sorted;
+        }
+        if (url === "?") {
+            url = "";
+        }
+        console.log(sorted)
+        console.log(url);
+        const response = await fetch(`http://localhost:8080/is-lab1-backend-1.0-SNAPSHOT/api/import/history${url}`, {
             method: "GET",
             headers: {
-                "Content-Type": "application/json",
+                "Accept": "application/json",
                 Authorization: `Bearer ${localStorage.getItem("token")}`,
             }
         });
-        const data = await response.json();
-        setHistory(data);
+        const updatedData = replaceNullWithZero(await response.json());
+        setHistory(updatedData);
     };
 
     useEffect(() => {
         fetchHistory();
     }, []);
 
-    return (
-        <div>
-            <h1>Import Objects</h1>
-            <input type="file" accept=".json" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload</button>
+    const fetchPaginationData = async () => {
+        console.log(currentPage);
+        console.log(typeof currentPage);
+        let filterColomn = document.getElementById("filter-coloumn");
+        let filter = document.querySelector("#filter");
+        let sorted = document.getElementById("sorted");
+        if (filterColomn && filter) {
+            filterColomn = filterColomn.value;
+            filter = filter.value;
+        }
+        if (sorted) {
+            sorted = sorted.value;
+        }
+        let url = "?";
+        if (filterColomn && filter) {
+            url += "&filter-value=" + filter + "&filter-column=" + filterColomn;
+        }
+        if (currentPage) {
+            url += "&page=" + currentPage;
+        }
+        if (sorted) {
+            url += "&sorted=" + sorted;
+        }
+        if (url === "?") {
+            url = "";
+        }
+        console.log(sorted)
+        console.log(url);
+        const response = await fetch(`http://localhost:8080/is-lab1-backend-1.0-SNAPSHOT/api/import/history${url}`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            }
+        });
+        const updatedData = replaceNullWithZero(await response.json());
+        setHistory(updatedData);
+    };
 
-            <h2>Import History</h2>
-            <button onClick={fetchHistory}>Show History</button>
-            <ul>
-                {history.map((record) => (
-                    <li key={record.id}>
-                        ID: {record.id}, Time: {record.importTime}, Status: {record.status}, User: {record.username}, Count: {record.numberOfImportedRecords}
-                    </li>
-                ))}
-            </ul>
-        </div>
+
+    return (
+        <>
+            <div className="show-table-container">
+                <table border="1" style={{width: "100%", textAlign: "left"}}>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>File Name</th>
+                        <th>Username</th>
+                        <th>Import Time</th>
+                        <th>Number of imported records</th>
+                        <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {history.map((item) => (<tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.fileName}</td>
+                        <td>{item.username}</td>
+                        <td>{item.importTime}</td>
+                        <td>{item.numberOfImportedRecords}</td>
+                        <td>{item.status}</td>
+                    </tr>))}
+                    </tbody>
+                </table>
+                <div className="showBlock">
+                    <label>Столбец для фильтрации:
+                        <select id="filter-coloumn">
+                            <option value="fileName">File Name</option>
+                            <option value="username">Username</option>
+                            <option value="importTime">Import Time</option>
+                            <option value="numberOfImportedRecords">Number of imported records</option>
+                            <option value="status">Status</option>
+                        </select>
+                    </label>
+                    <br/>
+                    <label className="text-field__label" htmlFor="filter"
+                    >Фильтрация:
+                    </label>
+                    <input
+                        className="text-field__input ticket"
+                        placeholder="filter"
+                        type="text"
+                        id="filter"
+                    />
+                    <br/>
+                    <label>Столбец для сортировки:
+                        <select id="sorted">
+                            <option value="id">ID</option>
+                            <option value="fileName">File Name</option>
+                            <option value="username">Username</option>
+                            <option value="importTime">Import Time</option>
+                            <option value="numberOfImportedRecords">Number of imported records</option>
+                            <option value="status">Status</option>
+                        </select>
+                    </label>
+                    <br/>
+                    <div style={{marginTop: "20px", display: "flex", justifyContent: "center"}}>
+                        <button
+                            onClick={goToPreviousPage}
+                            disabled={currentPage === 1}
+                            style={{marginRight: "10px", background: "#1a1a1a", paddingTop: "0px"}}
+                        >
+                            Previous
+                        </button>
+                        <span>Page {currentPage}</span>
+                        <button
+                            onClick={goToNextPage}
+                            style={{marginLeft: "10px", background: "#1a1a1a", paddingTop: "0px"}}
+                        >
+                            Next
+                        </button>
+                    </div>
+                    <button className="showBtn" onClick={fetchPaginationData}>Show</button>
+                </div>
+
+                <div className="deleteBlock">
+                    <div>Import File</div>
+                    <input type="file" accept=".json" onChange={handleFileChange}/>
+                    <br/>
+                    <br/>
+                    <button className="showBtn" onClick={handleUpload}>Upload</button>
+                </div>
+            </div>
+        </>
     );
 }
 
